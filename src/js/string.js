145b43e689a8b12bb271318cd4049896280e2316
Frafaelrls <frafaelrls@gmail.com>
2023-05-02 20:14:48 -0300

    adicionando o numero do cartao ao html

--- src/js/string.js
+++ src/js/string.js
@@ -9,6 +9,11 @@ const nomeCompletoStringPadding = sobrenome.padStart(14, nome);
 
 document.getElementById('stringPad').innerHTML = nomeCompletoStringPadding;
 
+const numeroCartao = '8759';
+
+// Usando padStart para completar uma string até que atinja um número de caractéres
+document.getElementById('numeroCartao').innerHTML = numeroCartao.padStart(16, '*');
+
 // padend adiciona um sufixo a string, informando o número final de caractéres
 
 const nomeCompletoPadEnd = nome.padEnd(14, sobrenome);
